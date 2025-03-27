eb1990e1b2d32a907445072d19627cb41c9b20bb
Jonas Bark <jonas.bark@gmx.de>
2025-03-27 16:57:02 +0100

    integrate flutter blue plus windows

--- flutter_blue_plus_windows/lib/flutter_blue_plus_windows.dart
+++ flutter_blue_plus_windows/lib/flutter_blue_plus_windows.dart
@@ -4,4 +4,4 @@ export 'package:flutter_blue_plus/flutter_blue_plus.dart' hide FlutterBluePlus;
 export 'package:win_ble/win_ble.dart';
 export 'package:win_ble/win_file.dart';
 
-export 'src/flutter_blue_plus_windowss.dart';
+export 'src/flutter_blue_plus_windows.dart';
