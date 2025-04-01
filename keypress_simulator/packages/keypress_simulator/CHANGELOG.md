3d414edda947eac70a812c1eef91574317673542
Jonas Bark <jonas.bark@gmx.de>
2025-04-01 21:17:46 +0200

    Zwift Ride: cleanup, fix button

--- keypress_simulator/packages/keypress_simulator/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 0.2.0
-
-* feat: Convert to federated plugin
-
-## 0.1.0
-
-* First release.
--- keypress_simulator/packages/keypress_simulator_platform_interface/lib/keypress_simulator_platform_interface.dart
+++
@@ -1,4 +0,0 @@
-library keypress_simulator_platform_interface;
-
-export 'src/keypress_simulator_method_channel.dart';
-export 'src/keypress_simulator_platform_interface.dart';
