132b5754475d5bd961f8f79c6ddb0dbf3c3b29c7
renoua <50181861+renoua@users.noreply.github.com>
2025-08-22 18:51:05 +0200

    Update base_device.dart

--- lib/bluetooth/devices/base_device.dart
+++ lib/bluetooth/devices/base_device.dart
@@ -271,10 +271,10 @@ abstract class BaseDevice {
   Future<List<ZwiftButton>?> processClickNotification(Uint8List message);
 
   Future<void> _performActions(List<ZwiftButton> buttonsClicked, bool repeated) async {
-    /* if (!repeated &&
+    if (!repeated &&
         buttonsClicked.any(((e) => e.action == InGameAction.shiftDown || e.action == InGameAction.shiftUp))) {
-      await _vibrate();
-    } */ 
+      // await _vibrate();
+    } 
   
     // Appuis en cours : tous ceux qui sont actuellement envoyés
     final newlyPressed = buttonsClicked.toSet();
